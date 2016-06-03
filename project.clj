6c24601338f89b1463c2cb8cd7da9910163abafc
Arnauld <arnauld.loyer@gmail.com>
2016-06-03 10:28:36 +0200

    initial layout

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject jarvis "0.1.0-SNAPSHOT"
+  :description "JARVIS - my personal assistant"
+  :url "http://technbolts.org/jarvis"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
---
+++ test/jarvis/core_test.clj
@@ -0,0 +1,7 @@
+(ns jarvis.core-test
+  (:require [clojure.test :refer :all]
+            [jarvis.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail not anymore..."
+    (is (= 1 1))))
