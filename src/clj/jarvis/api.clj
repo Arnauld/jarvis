1a66a5ff11da2bb01b2a04904abae6ba5da0a416
Arnauld <arnauld.loyer@gmail.com>
2016-06-12 23:51:27 +0200

    events have spec too

--- src/clj/jarvis/api.clj
+++ src/clj/jarvis/api.clj
@@ -1,17 +1,27 @@
 (ns jarvis.api
   (require [clojure.spec :as s]
            [jarvis.event-bus :as event-bus])
-  (:import (java.util UUID)))
+  (:import (java.util UUID Date)))
 
 (def event-bus (event-bus/create-in-memory))
 
 (def event-store ())
 
-(defn next-id []
+(defn new-id []
   (UUID/randomUUID))
+
+(defn now []
+  (Date.))
 
 (defn schedule-reminder [ref schedule]
   (println "scheduling" ref "in" schedule))
+
+(defn- validate-or-fail [spec what]
+  (let [parsed (s/conform spec what)]
+    (if (= parsed ::s/invalid)
+      (let [explanation (s/explain-data spec what)]
+        (throw (ex-info (str "Invalid input: " explanation) explanation)))
+      what)))
 
 ;;---------------------------------------------------------
 ;;
@@ -19,15 +29,33 @@
 ;;
 ;;---------------------------------------------------------
 
-(s/def :jarvis.api.ref/id string?)
-(s/def :jarvis.api.ref/type #{:task :person :event :comment})
-(s/def ::ref (s/keys :req-un [:jarvis.api.ref/type :jarvis.api.ref/id]))
+(s/def :jarvis.ref/id uuid?)
+(s/def :jarvis.ref/type #{:task :person :event :comment})
+(s/def :jarvis.ref/spec (s/keys :req-un [:jarvis.ref/type :jarvis.ref/id]))
 
 (defrecord Ref [type id])
 
 (defn new-ref [type id]
-  {:post [(s/valid? ::ref %)]}
+  {:post [(s/valid? :jarvis.ref/spec %)]}
   (Ref. type id))
+
+;;---------------------------------------------------------
+;;
+;; EVENT
+;;
+;;---------------------------------------------------------
+
+(s/def :jarvis.event/ref :jarvis.ref/spec)
+(s/def :jarvis.event/type keyword?)
+(s/def :jarvis.event/timestamp inst?)
+(s/def :jarvis.event/payload #(not (nil? %)))
+(s/def :jarvis.event/spec (s/keys :req-un [:jarvis.event/ref :jarvis.event/type :jarvis.event/timestamp :jarvis.event/payload]))
+
+(defrecord Event [ref type timestamp payload])
+
+(defn new-event [ref type
+                 & {:keys [payload timestamp] :or {payload {} timestamp (now)}}]
+  (validate-or-fail :jarvis.event/spec (Event. ref type timestamp payload)))
 
 ;;---------------------------------------------------------
 ;;
@@ -35,34 +63,35 @@
 ;;
 ;;---------------------------------------------------------
 
-(s/def :task/status #{:terminated
-                      :cancelled
-                      :delayed
-                      :pending
-                      :in-progress
-                      :not-started})
-(s/def :task/name string?)
-(s/def :task/description string?)
+(s/def :jarvis.task/ref :jarvis.ref/spec)
+(s/def :jarvis.task/status #{:terminated
+                             :cancelled
+                             :delayed
+                             :pending
+                             :in-progress
+                             :not-started})
+(s/def :jarvis.task/name string?)
+(s/def :jarvis.task/description string?)
+(s/def :jarvis.task/priority #{:low :normal :high})
+(s/def :jarvis.task/spec (s/keys :req-un [:jarvis.task/ref :jarvis.task/name]
+                                 :opt-un [:jarvis.task/description :jarvis.task/status :jarvis.task/priority]))
 
 (defn should-schedule-task? [task-status]
   (not (contains? [:terminated :cancelled] task-status)))
 
-(defn new-task [^String name
-                ^String description
-                & {:keys [sub-task-of reminder priority status] :or {priority    100
-                                                                     sub-task-of nil
-                                                                     status      :status/not-started}}]
-  (let [task-ref (Ref. :task (next-id))
+(defn create-task [^String name
+                   ^String description
+                   & {:keys [sub-task-of reminder priority status] :or {priority    100
+                                                                        sub-task-of nil
+                                                                        status      :status/not-started}}]
+  (let [task-ref (new-ref :task (new-id))
         task {:ref              task-ref
               :task/name        name
               :task/description description
               :task/priority    priority
-              :task/status      status
-              :linked-to        (if sub-task-of
-                                  [{:task/ref sub-task-of :relation :subtask}]
-                                  [])}
+              :task/status      status}
         schedule (or reminder :time/tomorrow)]
-    (when (and (should-schedule-task? status)
-               (not (= schedule :time/none)))
+    (if (and (should-schedule-task? status)
+             (not (= schedule :time/none)))
       (schedule-reminder task-ref schedule))
     task-ref))
--- test/jarvis/api_test.clj
+++ test/jarvis/api_test.clj
@@ -1,11 +1,29 @@
 (ns jarvis.api-test
   (:require [clojure.test :refer :all]
-            [jarvis.api :refer :all]))
+            [jarvis.api :refer :all])
+  (:import (java.util UUID Date)))
+
+(defn- after-or-equals? [date1 date2]
+  "Test if date2 is after or equals to date1"
+  (or (.after date2 date1)
+      (.equals date2 date1)))
 
 (deftest ref-test
   (testing "ref specs - supported creation"
-    (is
-      (= (->Ref :task "az") (new-ref :task "az"))))
+    (let [ref (new-ref :task (new-id))]
+      (is (= :task (:type ref)))
+      (is (instance? UUID (:id ref)))))
   (testing "ref specs - invalid creation"
-    (is
-      (thrown? AssertionError (new-ref :task :az)))))
+    (is (thrown? AssertionError (new-ref :task :az)))))
+
+(deftest event-test
+  (testing "event specs - supported creation"
+    (let [ref (new-ref :task (new-id))
+          nowP (now)
+          event (new-event ref :task/created)
+          nowA (now)]
+      (is (= ref (:ref event)))
+      (is (= :task/created (:type event)))
+      (is (after-or-equals? nowP (:timestamp event)))
+      (is (after-or-equals? (:timestamp event) nowA)
+          ))))
